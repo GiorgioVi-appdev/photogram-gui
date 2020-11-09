cdfe8dc0274bb8baa74010b91d435279fb0bb6d2
GiorgioVi <gvidali@uchicago.edu>
2020-11-09 05:22:44 +0000

    we doin it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-
+  get("/",{:controller => "users", :action=> "index"})
   get("/users",{:controller => "users", :action=> "index"})
+  get("/users/:path_name",{:controller => "users", :action=> "show"})
+  get("/photos",{:controller => "photos", :action=> "index"})
+  get("/photos/:path_id",{:controller => "photos", :action=> "show"})
+  get("/delete_photo/:toast_id",{:controller => "photos", :action=> "baii"})
+  get("/insert_photo",{:controller => "photos", :action=> "create"})
+
 
 end
